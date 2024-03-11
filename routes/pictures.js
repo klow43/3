9d1f0e2f8afe19b710f8de585dbe2a45fdbb9067
Karen Beate <karen.beate@outlook.com>
2024-03-11 20:42:37 +0100

    apply use of AWS Bucket

--- routes/pictures.js
+++ routes/pictures.js
@@ -2,10 +2,29 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const AWS = require('aws-sdk');
+require('aws-sdk/lib/maintenance_mode_message').supress = true;
+const s3 = new AWS.S3();
 
 //Get pictures listing.
-router.get('/', function(req, res, next) {
-    const pictures = fs.readdirSync(path.join(__dirname, '../pictures/'));
+router.get('/', async function(req, res, next) {
+    const params = {
+        Bucket : process.env.CYCLIC_BUCKET_NAME,
+        Delimiter : '/',
+        Prefix : 'public/'
+    };
+    const allObjects = await s3.listObjects(params).promise();
+    const keys = allObjects?.Contents.map( x => x.Key );
+    const pictures = await Promise.all(keys.map( async (key) => {
+        let my_file = await s3.getObject({
+            Bucket : process.env.CYCLIC_BUCKET_NAME,
+            Key : key,
+        }).promise();
+        return {
+            src : Buffer.from(my_file.Body).toString('base64'),
+            name : key.split('/'.toUpperCase())
+        }
+    })) 
     res.render('pictures', { pictures : pictures})
 });
 
@@ -18,9 +37,13 @@ router.get('/:pictureName', function(req, res, next) {
 })
 
 //post new file
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
     const file = req.files.file;
-    fs.writeFileSync(path.join(__dirname, '../pictures/', file.name), file.data);  
+    await s3.putObject({
+        Body : file.data,
+        Bucket : process.env.CYCLIC_BUCKET_NAME,
+        Key : 'public/' + file.name,
+    }).promise();
     res.end();
 });
 
