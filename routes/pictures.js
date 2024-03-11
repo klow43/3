1b1d5b7b967307c1a46d4f96aca627450d9423d6
Karen Beate <karen.beate@outlook.com>
2024-03-11 18:44:13 +0100

    /pictures/:pictureName

--- routes/pictures.js
+++ routes/pictures.js
@@ -9,6 +9,15 @@ router.get('/', function(req, res, next) {
     res.render('pictures', { pictures : pictures})
 });
 
+//display file of params.
+router.get('/:pictureName', function(req, res, next) {
+    const pictureName = req.params.pictureName;
+    const pictures = fs.readdirSync(path.join(__dirname, '../pictures/'));
+    let result = pictures.filter(picture => picture == pictureName)
+    res.render('pictures', { pictures : result })
+})
+
+//post new file
 router.post('/', function(req, res, next) {
     const file = req.files.file;
     fs.writeFileSync(path.join(__dirname, '../pictures/', file.name), file.data);  
